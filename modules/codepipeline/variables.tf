b4e44075a6101b507b114196c1538cc5063c7382
Vladyslav Kosheliev <vladyslav.kosheliev@meteogroup.com>
2019-03-22 16:21:33 +0100

    Add missing variables

--- modules/codepipeline/variables.tf
+++ modules/codepipeline/variables.tf
@@ -9,9 +9,9 @@ variable "stage" {
   description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
 }
 
-variable "enabled" {
+variable "cache_enabled" {
   default     = "true"
-  description = "Enable ``CodePipeline`` creation"
+  description = "Enable creation of cache bucket"
 }
 
 variable "infra_build" {
@@ -77,11 +77,6 @@ variable "tags" {
   description = "Additional tags (e.g. `map('Project', 'ABC')`"
 }
 
-variable "privileged_mode" {
-  default     = "false"
-  description = "If set to true, enables running the Docker daemon inside a Docker container on the CodeBuild instance. Used when building Docker images"
-}
-
 variable "aws_region" {
   type        = "string"
   default     = ""
@@ -115,4 +110,76 @@ variable "environment_variables" {
   }]
 
   description = "A list of maps, that contain both the key 'name' and the key 'value' to be used as additional environment variables for the build."
+}
+
+variable "cache_expiration_days" {
+  type        = "string"
+  default     = "7"
+  description = "How many days should the build cache be kept"
+}
+
+variable "cache_bucket_suffix_enabled" {
+  type        = "string"
+  default     = "true"
+  description = "The cache bucket generates a random 13 character string to generate a unique bucket name. If set to false it uses terraform-null-label's id value"
+}
+
+variable "badge_enabled" {
+  type        = "string"
+  default     = "false"
+  description = "Generates a publicly-accessible URL for the projects build badge. Available as badge_url attribute when enabled."
+}
+
+variable "build_image" {
+  type        = "string"
+  default     = "aws/codebuild/docker:1.12.1"
+  description = "Docker image for build environment, e.g. 'aws/codebuild/docker:1.12.1' or 'aws/codebuild/eb-nodejs-6.10.0-amazonlinux-64:4.0.0'. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref.html"
+}
+
+variable "build_compute_type" {
+  type        = "string"
+  default     = "BUILD_GENERAL1_SMALL"
+  description = "Instance type of the build instance"
+}
+
+variable "build_timeout" {
+  type        = "string"
+  default     = "60"
+  description = "How long in minutes, from 5 to 480 (8 hours), for AWS CodeBuild to wait until timing out any related build that does not get marked as completed."
+}
+
+variable "buildspec" {
+  type        = "string"
+  default     = ""
+  description = "Optional buildspec declaration to use for building the project"
+}
+
+variable "privileged_mode" {
+  type        = "string"
+  default     = "false"
+  description = "(Optional) If set to true, enables running the Docker daemon inside a Docker container on the CodeBuild instance. Used when building Docker images"
+}
+
+variable "source_type" {
+  type        = "string"
+  default     = "CODEPIPELINE"
+  description = "The type of repository that contains the source code to be built. Valid values for this parameter are: CODECOMMIT, CODEPIPELINE, GITHUB, GITHUB_ENTERPRISE, BITBUCKET or S3."
+}
+
+variable "source_location" {
+  type        = "string"
+  default     = ""
+  description = "The location of the source code from git or s3."
+}
+
+variable "artifact_type" {
+  type        = "string"
+  default     = "CODEPIPELINE"
+  description = "The build output artifact's type. Valid values for this parameter are: CODEPIPELINE, NO_ARTIFACTS or S3."
+}
+
+variable "report_build_status" {
+  type        = "string"
+  default     = "false"
+  description = "Set to true to report the status of a build's start and finish to your source provider. This option is only valid when the source_type is BITBUCKET or GITHUB."
 }
