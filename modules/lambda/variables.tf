cd6f73a2f5600f566a10b9c32ef40201b950f170
Vladyslav Kosheliev <vladyslav.kosheliev@meteogroup.com>
2019-03-28 09:08:47 +0100

    Lambda common module

---
+++ modules/lambda/variables.tf
@@ -0,0 +1,60 @@
+variable "runtime" {
+  description = "Language to use for Lambda"
+  type        = "string"
+  default     = ""
+}
+
+variable "handler" {
+  description = "Program entrypoint for Lambda"
+  type        = "string"
+  default     = ""
+}
+
+variable "timeout" {
+  description = "Timeout after which Lamdba will terminate"
+  type        = "string"
+  default     = "10"
+}
+
+variable "source_bucket" {
+  description = "Bucket to use for loading Lambda source ZIP"
+  type        = "string"
+}
+
+variable "source_prefix" {
+  description = "S3 prefix to use for loading Lambda ZIP"
+  type        = "string"
+  default     = ""
+}
+
+variable "source_version" {
+  description = "Version of Lambda ZIP to use"
+  type        = "string"
+}
+
+variable "function_name" {
+  description = "Name for Lambda function"
+  type        = "string"
+}
+
+variable "environment_variables" {
+  description = "Variables to provide for Lambda environment"
+  type        = "map"
+  default     = {}
+}
+
+variable "access_policy_document" {
+  description = "IAM policy provided to Lambda role"
+  type        = "string"
+}
+
+variable "source_types" {
+  description = "Source types which are allowed to invoke the Lambda. Must align with entries in source_arns variable"
+  type        = "list"
+  default     = [""]
+}
+
+variable "source_arns" {
+  description = "Source ARNs which are allowed to invoke the Lambda. Must align with entries in source_types variable"
+  type        = "list"
+}
