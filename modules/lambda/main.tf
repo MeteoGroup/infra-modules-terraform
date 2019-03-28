9ae758f423e1d9697cf78053ef0fde2dfd8c6839
Vladyslav Kosheliev <vladyslav.kosheliev@meteogroup.com>
2019-03-28 12:30:23 +0100

    Change statement id to number

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -32,7 +32,7 @@ resource "aws_lambda_permission" "allow_source" {
   function_name = "${aws_lambda_function.this.function_name}"
 
   #statement_id  = "AllowExecutionFrom${replace(title(var.source_types[count.index]), "." , "-")}"
-  statement_id = "${var.source_types[count.index]}"
+  statement_id = "${count.index}"
   action       = "lambda:InvokeFunction"
   principal    = "${var.source_types[count.index]}.amazonaws.com"
   source_arn   = "${var.source_arns[count.index]}"
