3c3fb79b20745ff349107af12161ecc25efa795c
Vladyslav Kosheliev <vladyslav.kosheliev@meteogroup.com>
2019-03-28 12:00:32 +0100

    Replace dots

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -30,7 +30,7 @@ resource "aws_lambda_function" "this" {
 
 resource "aws_lambda_permission" "allow_source" {
   function_name = "${aws_lambda_function.this.function_name}"
-  statement_id  = "AllowExecutionFrom${title(var.source_types[count.index])}"
+  statement_id  = "AllowExecutionFrom${replace(title(var.source_types[count.index]), '.', '-')}"
   action        = "lambda:InvokeFunction"
   principal     = "${var.source_types[count.index]}.amazonaws.com"
   source_arn    = "${var.source_arns[count.index]}"
