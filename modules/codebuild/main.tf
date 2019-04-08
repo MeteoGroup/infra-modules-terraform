3eb9f6e21a3c813dd0012cb09e00d47aa26c17d2
Vladyslav Kosheliev <vladyslav.kosheliev@meteogroup.com>
2019-04-08 11:46:46 +0200

    Add image_pull_credentials_type SERVICE_ROLE

--- modules/codebuild/main.tf
+++ modules/codebuild/main.tf
@@ -217,10 +217,11 @@ resource "aws_codebuild_project" "within_vpc" {
   cache = ["${local.cache}"]
 
   environment {
-    compute_type    = "${var.build_compute_type}"
-    image           = "${var.build_image}"
-    type            = "LINUX_CONTAINER"
-    privileged_mode = "${var.privileged_mode}"
+    compute_type                = "${var.build_compute_type}"
+    image                       = "${var.build_image}"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = "${var.privileged_mode}"
+    image_pull_credentials_type = "SERVICE_ROLE"
 
     environment_variable = [{
       "name"  = "AWS_REGION"
