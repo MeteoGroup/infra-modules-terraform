b5c8b34c8e5c14042c48786d5710616e63150cb7
Vladyslav Kosheliev <vladyslav.kosheliev@meteogroup.com>
2019-04-08 12:44:50 +0200

    nvalid input: cannot use a CodeBuild curated image with imagePullCredentialsType SERVICE_ROLE

--- modules/codepipeline/codebuild.tf
+++ modules/codepipeline/codebuild.tf
@@ -404,11 +404,12 @@ resource "aws_codebuild_project" "default" {
   cache = ["${local.cache}"]
 
   environment {
-    compute_type                = "${var.build_compute_type}"
-    image                       = "${var.build_image}"
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = "${var.privileged_mode}"
-    image_pull_credentials_type = "SERVICE_ROLE"
+    compute_type    = "${var.build_compute_type}"
+    image           = "${var.build_image}"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = "${var.privileged_mode}"
+
+    # image_pull_credentials_type = "SERVICE_ROLE"
 
     environment_variable = [{
       "name"  = "AWS_REGION"
