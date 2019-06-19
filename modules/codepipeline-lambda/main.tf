3b3bb291e9d9668a57a4684cfd6b71207fc44450
Vladyslav Kosheliev <vladyslav.kosheliev@meteogroup.com>
2019-06-19 10:45:28 +0200

    Lambda codepipeline module

---
+++ modules/codepipeline-lambda/main.tf
@@ -0,0 +1,99 @@
+data "aws_iam_role" "role_codepipeline" {
+  name = "codepipeline-role"
+}
+
+data "aws_ssm_parameter" "token" {
+  name = "/maersk-repo-primary/oauth-token"
+}
+
+resource "aws_s3_bucket" "codepipeline" {
+  bucket        = "${var.name_prefix}"
+  acl           = "private"
+  force_destroy = true
+  tags          = "${var.tags}"
+}
+
+resource "aws_codepipeline" "source_build_deploy" {
+  name = "${var.name_prefix}"
+
+  role_arn = "${data.aws_iam_role.role_codepipeline.arn}"
+
+  artifact_store {
+    location = "${aws_s3_bucket.codepipeline.bucket}"
+    type     = "S3"
+  }
+
+  stage {
+    name = "Get-Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "ThirdParty"
+      provider         = "GitHub"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration {
+        OAuthToken           = "${data.aws_ssm_parameter.token.value}"
+        Owner                = "${var.repo_owner}"
+        Repo                 = "${var.repo_name}"
+        Branch               = "${var.branch}"
+        PollForSourceChanges = "${var.poll_source_changes}"
+      }
+    }
+  }
+
+  stage {
+    name = "Lambda-source-code-test"
+
+    action {
+      name     = "Build"
+      category = "Build"
+      owner    = "AWS"
+      provider = "CodeBuild"
+      version  = "1"
+
+      input_artifacts = ["source_output"]
+
+      configuration {
+        ProjectName = "${var.repo_name}-test"
+      }
+    }
+  }
+
+  stage {
+    name = "Build-Lambda"
+
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["package"]
+
+      configuration {
+        ProjectName = "${var.repo_name}-image"
+      }
+    }
+  }
+
+  stage {
+    name = "Update-Lambda"
+
+    action {
+      name            = "UpdateLambdaAuth"
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      version         = "1"
+      input_artifacts = ["package"]
+
+      configuration {
+        ProjectName = "${var.repo_name}-lambda${var.prod_prefix}"
+      }
+    }
+  }
+}
---
+++ modules/codepipeline-lambda/variables.tf
@@ -0,0 +1,41 @@
+variable "name_prefix" {
+  description = ""
+  type        = "string"
+  default     = ""
+}
+
+variable "prod_prefix" {
+  description = "Prefix for last codebuild name in Pipeline"
+  type        = "string"
+  default     = ""
+}
+
+variable "repo_owner" {
+  description = ""
+  type        = "string"
+  default     = "MeteoGroup"
+}
+
+variable "repo_name" {
+  description = ""
+  type        = "string"
+  default     = ""
+}
+
+variable "branch" {
+  description = ""
+  type        = "string"
+  default     = ""
+}
+
+variable "poll_source_changes" {
+  description = ""
+  type        = "string"
+  default     = "false"
+}
+
+variable "tags" {
+  description = ""
+  type        = ""
+  default     = {}
+}
