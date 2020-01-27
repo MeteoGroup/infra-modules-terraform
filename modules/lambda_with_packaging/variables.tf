15a006b46977b71ef36623c64c66365ef72ab63a
Mikalai Huryn <Mikalai_Huryn@epam.com>
2020-01-27 19:38:08 +0300

    Add fix for vpc_config in lambda vars

--- modules/lambda_with_packaging/variables.tf
+++ modules/lambda_with_packaging/variables.tf
@@ -64,7 +64,7 @@ variable "source_arns" {
 
 variable "vpc_config" {
   description = "Provide this to allow your function to access your VPC (if both 'subnet_ids' and 'security_group_ids' are empty then vpc_config is considered to be empty or unset, see https://docs.aws.amazon.com/lambda/latest/dg/vpc.html for details)."
-  type        = "map"
+  type        = "list"
 
   default = [{
     security_group_ids = []
